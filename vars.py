4e11bf2b32119574f42ae2bc8c8ae094c0ae04fe
Kalamuwu <kalamariking@protonmail.com>
2023-03-22 19:57:47 -0400

    Added vars.py
    No idea why this wouldn't push through VSCode git.

---
+++ vars.py
@@ -0,0 +1,30 @@
+author_id = 594154260271464458
+verified_role = 959422522053521449
+
+otp_email_text = """In order to gain access to the rest of the server, we have to verify that the account !name belongs to you.\n
+If you did not join the Pinkerton server, or do not know what this is about, disregard this message; nobody has access to your discord or google account and nothing has gone wrong.\n
+If you DID just join the Pinkerton server, and you are prompted for a OTP (one-time password), your code is:\n   !otp   \n
+Please note this is not case-sensitive, but only input these 8 characters."""
+otp_email_html = """
+<!-- EMAIL-START -->
+<!DOCTYPE html>
+<html>
+<body>
+<div style='margin: 8%; margin-top: 40px; padding: 4%; box-shadow: 0 4px 8px 0 #cccccc, 0 6px 20px 0 #cfcfcf; border: thin solid #a8acad; border-radius: 15px; font: 15px arial; color: #272829; background-color: #f7f7f8;'>
+  <center>
+    <h3>In order to gain access to the rest of the server, we have to verify that the account !name belongs to you.</h3>
+    <br>
+    <em style='font-size: 13px; color: #363838;'>If you did not join the Pinkerton server, or do not know what this is about, <strong>disregard this message; nobody has access to your discord or google account and nothing has gone wrong.</strong></em>
+    <br><br>
+    <h3>If you DID just join the Pinkerton server, and you are prompted for an OTP (one-time password), <strong>your code is:</strong>
+    </h3>
+    <br><br>
+    <code style='color: #272829; font-size: 25px; text-align: center; float: center; padding: 10px; background: #989b9c; border-radius: 15px; width: 8em; white-space: pre-wrap; margin: 1.8em 0 1.8em 0;'>!otp</code>
+    <br><br><br>
+    <em style='font-size: 12px;'>Please note this is not case-sensitive, but please only input these 8 characters. For example, <code style='background-color: #e0e0e0; border-radius: 6px; padding: 1px 4px 1px 4px;'>a</code> and <code style='background-color: #e0e0e0; border-radius: 6px; padding: 1px 4px 1px 4px;'>A</code> will be counted as the same character. </em>
+  </center>
+</div>
+</body>
+</html>
+<!-- EMAIL-END -->
+"""
