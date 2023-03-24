03b1f2d1c8bed7d874e9e84b778496e72487511d
Caleb Thomas <64808289+TSBsebseb@users.noreply.github.com>
2023-03-24 10:16:25 -0400

    int for gbt testing with api keys

--- cogs/ChatGBT.py
+++ cogs/ChatGBT.py
@@ -2,10 +2,12 @@ import disnake
 import openai
 from disnake.ext import commands
 
-
+tempenv = "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"
 class ChatGBT(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+
+
 
 
 
